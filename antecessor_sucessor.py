d2a76a8f14ecf5b77aa42d238877af56f8741d39
AlyneMeneses <133061715+AlyneMeneses@users.noreply.github.com>
2025-02-20 14:15:33 -0300

    Delete antecessor_sucessor.py

--- antecessor_sucessor.py
+++
@@ -1,7 +0,0 @@
-# PROGRAMA QUE LEIA UM NUMERO INTEIRA E MOSTRE NA TELA O SEU SUCESSOR E SEU ANTECESSOR.
-
-
-n = int(input('Digite um numero inteiro:'))
-a = n - 1
-s = n + 1
-print(f'O antecessor de {n} é {a}, o sucessor é {s}')
